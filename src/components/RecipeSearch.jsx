a906914c52edf325515c5b68c783144ef12e1274
JoepKamminga <joep_k@hotmail.nl>
2025-03-25 14:32:29 +0100

    Update RecipeSearch.jsx
    updated code to Search.

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -8,17 +8,17 @@ export const RecipeSearch = ({ gefilterdeReceptenInstellen }) => {
   // console.log("recipe.label:", data.hits);
 
   const handleChange = (event) => {
-    setSearchField(event.target.value);
+    const zoekWaarde = event.target.value;
+    setSearchField(zoekWaarde);
     const matchedRecipes = data.hits.filter(({ recipe }) => {
-      return recipe.label.toLowerCase().includes(searchField.toLowerCase());
+      return recipe.label.toLowerCase().includes(zoekWaarde.toLowerCase());
     });
-    //console.log("matchedRecipes:", matchedRecipes);
-    gefilterdeReceptenInstellen(matchedRecipes);
-  };
 
-  return (
-    <>
-      <TextInput onChange={handleChange} w={["80%", "35%"]} mb={8} />
-    </>
-  );
+    return (
+      <>
+        <TextInput onChange={handleChange} w={["80%", "35%"]} mb={8} />
+      </>
+    );
+  };
 };
+
