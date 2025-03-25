d949a7149b09bc049825c38e2f129791c22612c0
JoepKamminga <joep_k@hotmail.nl>
2025-03-25 14:58:45 +0100

    Update RecipeSearch.jsx
    feedback verwerkt.

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -5,20 +5,18 @@ import { TextInput } from "./ui/TextInput";
 export const RecipeSearch = ({ gefilterdeReceptenInstellen }) => {
   const [searchField, setSearchField] = useState("");
 
-  // console.log("recipe.label:", data.hits);
-
   const handleChange = (event) => {
     const zoekWaarde = event.target.value;
     setSearchField(zoekWaarde);
     const matchedRecipes = data.hits.filter(({ recipe }) => {
       return recipe.label.toLowerCase().includes(zoekWaarde.toLowerCase());
     });
-
-    return (
-      <>
-        <TextInput onChange={handleChange} w={["80%", "35%"]} mb={8} />
-      </>
-    );
+    gefilterdeReceptenInstellen(matchedRecipes);
   };
-};
 
+  return (
+    <>
+      <TextInput onChange={handleChange} w={["80%", "35%"]} mb={8} />
+    </>
+  );
+};
